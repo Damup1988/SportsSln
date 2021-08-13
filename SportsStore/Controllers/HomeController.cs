28af041d66531f25f138014860d5c4fe962a8c24
Damup1988 <damup1988@gmail.com>
2021-08-14 01:32:46 +0300

    Chapter 7 - Preparing the Controller

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Repository;
 
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
+        private IStoreRepository _repository;
+
+        public HomeController(IStoreRepository repository)
+        {
+            _repository = repository;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_repository.Products);
         }
     }
 }
