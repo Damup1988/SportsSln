30c1775f9190d6d9ffa49d470abf3a011be73a01
Damup1988 <damup1988@gmail.com>
2021-08-14 02:13:16 +0300

    Chapter 7 - Adding Pagination

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using SportsStore.Repository;
 
 namespace SportsStore.Controllers
@@ -6,14 +7,18 @@ namespace SportsStore.Controllers
     public class HomeController : Controller
     {
         private IStoreRepository _repository;
+        public int PageSize = 4;
 
         public HomeController(IStoreRepository repository)
         {
             _repository = repository;
         }
-        public IActionResult Index()
+        public ViewResult Index(int productPage = 1)
         {
-            return View(_repository.Products);
+            return View(_repository.Products.OrderBy(p =>
+                p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize));
         }
     }
 }
