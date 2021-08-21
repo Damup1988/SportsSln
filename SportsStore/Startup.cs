67a80e27bb7a3d635de79629c1805339ac3ac86a
Damup1988 <damup1988@gmail.com>
2021-08-21 12:33:54 +0300

    Chapter 7 - Improving the URLs

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -45,6 +46,8 @@ namespace SportsStore
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute("pagination", "Product/Page{productPage}",
+                    new {Controller = "Home", Action = "Index"});
                 endpoints.MapDefaultControllerRoute();
             });
             SeeData.EnsurePopulated(app);
