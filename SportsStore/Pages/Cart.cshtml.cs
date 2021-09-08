0b005e17489f4c7f856d1a1a60154cc81d006442
Damup1988 <damup1988@gmail.com>
2021-09-08 23:54:27 +0300

    Chapter 8 - Completing the Razor Page

---
+++ SportsStore/Pages/Cart.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SportsStore.Infrastructure;
+using SportsStore.Models;
+using SportsStore.Repository;
+
+namespace SportsStore.Pages
+{
+    public class CartModel : PageModel
+    {
+        private IStoreRepository _repository;
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+
+        public CartModel(IStoreRepository repo)
+        {
+            _repository = repo;
+        }
+
+        public void OnGet(string returnUrl)
+        {
+            ReturnUrl = returnUrl ?? "/";
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+        }
+
+        public IActionResult OnPost(long productId, string returnUrl)
+        {
+            Product product = _repository.Products.FirstOrDefault(p => p.ProductID == productId);
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            Cart.AddItem(product, 1);
+            HttpContext.Session.SetJson("cart", Cart);
+            return RedirectToPage(new {returnUrl = returnUrl});
+        }
+    }
+}
