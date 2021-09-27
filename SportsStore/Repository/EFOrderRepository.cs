5cc9cbc3b292ecaba726d28d47faae16400adff2
Damup1988 <damup1988@gmail.com>
2021-09-27 09:29:11 +0300

    Chapter 9 - Creating the Order Repository

---
+++ SportsStore/Repository/EFOrderRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
+namespace SportsStore.Repository
+{
+    public class EFOrderRepository : IOrderRepository
+    {
+        private StoreDbContext _context;
+
+        public IQueryable<Order> Orders => _context.Orders
+            .Include(o => o.Lines)
+            .ThenInclude(l => l.Product);
+
+        public EFOrderRepository(StoreDbContext ctx)
+        {
+            _context = ctx;
+        }
+        
+        public void SaveOrder(Order order)
+        {
+            _context.AttachRange(order.Lines.Select(l => l.Product));
+            if (order.OrderID == 0)
+            {
+                _context.Orders.Add(order);
+            }
+            _context.SaveChanges();
+        }
+    }
+}
