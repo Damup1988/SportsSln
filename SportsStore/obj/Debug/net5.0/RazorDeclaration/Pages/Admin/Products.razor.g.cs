615fd9f732e2c45cbb2bf6501d92f73a701170ec
Damup1988 <damup1988@gmail.com>
2021-10-21 00:00:33 +0300

    Chapter 10 - Creating the List Component

--- SportsStore/obj/Debug/net5.0/RazorDeclaration/Pages/Admin/Products.razor.g.cs
+++ SportsStore/obj/Debug/net5.0/RazorDeclaration/Pages/Admin/Products.razor.g.cs
@@ -53,15 +53,39 @@ using SportsStore.Models;
 #line default
 #line hidden
 #nullable disable
+#nullable restore
+#line 3 "D:\_ONEDRIVE\OneDrive\_Coding\_Projects\Freeman\SportsSln\SportsStore\Pages\Admin\Products.razor"
+using SportsStore.Repository;
+
+#line default
+#line hidden
+#nullable disable
     [Microsoft.AspNetCore.Components.RouteAttribute("/admin/products")]
     [Microsoft.AspNetCore.Components.RouteAttribute("/admin")]
-    public partial class Products : Microsoft.AspNetCore.Components.ComponentBase
+    public partial class Products : OwningComponentBase<SportsStore.Repository.IStoreRepository>
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
         {
         }
         #pragma warning restore 1998
+#nullable restore
+#line 42 "D:\_ONEDRIVE\OneDrive\_Coding\_Projects\Freeman\SportsSln\SportsStore\Pages\Admin\Products.razor"
+ 
+    public IStoreRepository Repository => Service;
+    public IEnumerable<Product> ProductData { get; set; }
+
+    protected async override Task OnInitializedAsync()
+    {
+        ProductData = await Repository.Products.ToListAsync();
+    }
+
+    public string GetDetailsUrl(long id) => $"/admin/products/details/{id}";
+    public string GetEditUrl(long id) => $"/admin/products/edit/{id}";
+
+#line default
+#line hidden
+#nullable disable
     }
 }
 #pragma warning restore 1591
